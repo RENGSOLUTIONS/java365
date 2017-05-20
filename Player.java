4123387d3d55f90faff53c9f0122134a95152335
liu, jay <liu.jay@acm.org>
2017-05-20 19:17:30 -0400

    initial class

---
+++ Player.java
@@ -0,0 +1,16 @@
+//please write a class called Player which has several data fields and a couple methods
+public class Player{
+	public int age;
+	public String name;
+	public int size;
+	public float score;
+	
+	public void compete(){
+	//do nothing
+	}
+
+	public float test(){
+		return score;
+	}
+
+}
